59c600384c7195d16e822883afd5c7c445fec4db
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2021-09-04 12:03:50 +0200

    exchange library load by explicit referencing package

--- R/rChain.R
+++ R/rChain.R
@@ -1,5 +1,3 @@
-library(openssl)
-library(uuid)
 
 rChain <- R6::R6Class(
   classname = "blockchain",
@@ -8,7 +6,7 @@ rChain <- R6::R6Class(
   public = list(
 
     initialize = function() {
-      private$keypair <- ec_keygen("P-521")
+      private$keypair <- openssl::ec_keygen("P-521")
       self$createBlock()
     },
 
@@ -18,10 +16,12 @@ rChain <- R6::R6Class(
       if (is.na(data)) stop("'data' is required to create a new item.")
       if (!is.character(data)) data <- as.character(data)
 
-      id <- UUIDgenerate(TRUE)
+      id <- uuid::UUIDgenerate(TRUE)
       ts <- as.POSIXlt(Sys.time(), "UTC")
-      ck <- as.character(sha512(paste(id, ts, data, sep = "+")))
-      ck <- base64_encode(signature_create(charToRaw(ck), sha512, private$keypair))
+      ck <- as.character(openssl::sha512(paste(id, ts, data, sep = "+")))
+      ck <- openssl::base64_encode(openssl::signature_create(charToRaw(ck),
+                                                             openssl::sha512,
+                                                             private$keypair))
       if (is.null(private$items_pool)) {
         private$items_pool <- as.data.frame(list(Id = id,
                                                  Timestamp = ts,
@@ -66,20 +66,20 @@ rChain <- R6::R6Class(
         it <- self$getItemPool()
         private$resetItemPool()
 
-        id <- UUIDgenerate(FALSE)
+        id <- uuid::UUIDgenerate(FALSE)
         ts <- as.POSIXlt(Sys.time(), "UTC")
         prt1 <- ifelse(private$last_block == 0L, NA, private$blocks[[private$last_block]]$Header$Id)
         prt2 <- ifelse(private$last_block == 0L, NA, private$blocks[[private$last_block]]$Header$Check)
-        hash <- as.character(sha512(paste(it$Check, collapse = "+")))
+        hash <- as.character(openssl::sha512(paste(it$Check, collapse = "+")))
         ck_str <- paste(id, ts, prt1, prt2, hash, sep="+")
-        ck <- signature_create(charToRaw(ck_str), sha512, private$keypair)
+        ck <- openssl::signature_create(charToRaw(ck_str), openssl::sha512, private$keypair)
 
         block <- list(Header = list(Seq = private$last_block + 1,
                                     Id = id,
                                     Timestamp = ts,
                                     Parent = prt1,
                                     Content = hash,
-                                    Check = base64_encode(ck)),
+                                    Check = openssl::base64_encode(ck)),
                       Body = it)
 
         private$blocks <- append(private$blocks, list(block))
@@ -94,11 +94,11 @@ rChain <- R6::R6Class(
       } else {
         for (i in 1:nrow(items)) {
           i <- items[i, ]
-          ck <- as.character(sha512(paste(i$Id, i$Timestamp, i$Data, sep = "+")))
-          chk_result <- try(signature_verify(charToRaw(ck),
-                                             base64_decode(i$Check),
-                                             sha512,
-                                             private$keypair$pubkey),
+          ck <- as.character(openssl::sha512(paste(i$Id, i$Timestamp, i$Data, sep = "+")))
+          chk_result <- try(openssl::signature_verify(charToRaw(ck),
+                                                      openssl::base64_decode(i$Check),
+                                                      openssl::sha512,
+                                                      private$keypair$pubkey),
                             silent = TRUE)
           if (class(chk_result) == "try-error"){
             cat("Failed signature validation for transaction. Id: ", i$Id, "\n")
@@ -120,10 +120,10 @@ rChain <- R6::R6Class(
                         ifelse(num == 1, NA, private$blocks[[num - 1]]$Header$Check),
                         private$blocks[[num]]$Header$Content,
                         sep="+")
-        chk_result <- try(signature_verify(charToRaw(ck_str),
-                                           base64_decode(private$blocks[[num]]$Header$Check),
-                                           sha512,
-                                           private$keypair$pubkey),
+        chk_result <- try(openssl::signature_verify(charToRaw(ck_str),
+                                                    openssl::base64_decode(private$blocks[[num]]$Header$Check),
+                                                    openssl::sha512,
+                                                    private$keypair$pubkey),
                           silent = TRUE)
         if (class(chk_result) == "try-error"){
           cat("Failed signature validation for block. Id: ", private$blocks[[num]]$Header$Id, "\n")
