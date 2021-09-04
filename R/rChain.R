883dd47b9c397c5490e625a9da4e350c7cd9e2e2
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2021-09-04 11:25:49 +0200

    convert enclosure to R6 class

--- R/rChain.R
+++ R/rChain.R
@@ -1,163 +1,168 @@
+library(openssl)
+library(uuid)
 
-rChainEnc <- function() {
-  library(openssl)
-  library(uuid)
-
-  items_pool <- NULL
-  blocks <- list()
-  last_block <- 0L
-  keypair <- ec_keygen("P-521")
+rChain <- R6::R6Class(
+  classname = "blockchain",
 
   # Exposed functions
-  addItem <- function(data = NULL) {
+  public = list(
 
-    if (is.null(data)) stop("'data' is required to create a new item.")
-    if (is.na(data)) stop("'data' is required to create a new item.")
-    if (!is.character(data)) data <- as.character(data)
+    initialize = function() {
+      private$keypair <- ec_keygen("P-521")
+      self$createBlock()
+    },
 
-    id <- UUIDgenerate(TRUE)
-    ts <- as.POSIXlt(Sys.time(), "UTC")
-    ck <- as.character(sha512(paste(id, ts, data, sep = "+")))
-    ck <- base64_encode(signature_create(charToRaw(ck), sha512, keypair))
-    if (is.null(items_pool)) {
-      items_pool <<- as.data.frame(list(Id = id,
-                                        Timestamp = ts,
-                                        Data = data,
-                                        Check = ck))
-    } else {
-      items_pool <<- rbind(items_pool, list(id, ts, data, ck))
-    }
-    return(list(Result=TRUE, Id=id))
-  }
+    addItem = function(data = NULL) {
 
-  getItemPool <- function() {
-    return(items_pool)
-  }
+      if (is.null(data)) stop("'data' is required to create a new item.")
+      if (is.na(data)) stop("'data' is required to create a new item.")
+      if (!is.character(data)) data <- as.character(data)
 
-  getBlocks <- function(num = NULL) {
-    if (is.null(num)) {
-      return(blocks)
-    } else {
-      if(!is.numeric(num)) stop("Block number expected to be 'numeric'")
-      if(length(num) != 1) stop("Block number expected to be a scalar")
-      if(num %% 1 != 0) stop("Block number expected to be an integer")
-      if(num > last_block) stop("Block number bigger than total number of blocks")
-      return(blocks[num])
-    }
-  }
-
-  getPubKey <- function() {
-    return(keypair$pubkey)
-  }
-
-  createBlock <- function() {
-    if (last_block == 0L) {
-      # Need to mint genesis block
-      addItem("Welcome to Genesis Block")
-    }
-
-    if (is.null(items_pool)) {
-      message("No new items to add to block.")
-    } else {
-
-      it <- getItemPool()
-      resetItemPool()
-
-      id <- UUIDgenerate(FALSE)
+      id <- UUIDgenerate(TRUE)
       ts <- as.POSIXlt(Sys.time(), "UTC")
-      prt1 <- ifelse(last_block == 0L, NA, blocks[[last_block]]$Header$Id)
-      prt2 <- ifelse(last_block == 0L, NA, blocks[[last_block]]$Header$Check)
-      hash <- as.character(sha512(paste(it$Check, collapse = "+")))
-      ck_str <- paste(id, ts, prt1, prt2, hash, sep="+")
-      ck <- signature_create(charToRaw(ck_str), sha512, keypair)
+      ck <- as.character(sha512(paste(id, ts, data, sep = "+")))
+      ck <- base64_encode(signature_create(charToRaw(ck), sha512, private$keypair))
+      if (is.null(private$items_pool)) {
+        private$items_pool <- as.data.frame(list(Id = id,
+                                                 Timestamp = ts,
+                                                 Data = data,
+                                                 Check = ck))
+      } else {
+        private$items_pool <- rbind(private$items_pool, list(id, ts, data, ck))
+      }
+      return(list(Result=TRUE, Id=id))
+    },
 
-      block <- list(Header = list(Seq = last_block + 1,
-                                  Id = id,
-                                  Timestamp = ts,
-                                  Parent = prt1,
-                                  Content = hash,
-                                  Check = base64_encode(ck)),
-                    Body = it)
+    getItemPool = function() {
+      return(private$items_pool)
+    },
 
-      blocks <<- append(blocks, list(block))
-      last_block <<- last_block + 1
-      return(list(Result = TRUE, Id = id, Seq = last_block))
-    }
-  }
+    getBlocks = function(num = NULL) {
+      if (is.null(num)) {
+        return(private$blocks)
+      } else {
+        if(!is.numeric(num)) stop("Block number expected to be 'numeric'")
+        if(length(num) != 1) stop("Block number expected to be a scalar")
+        if(num %% 1 != 0) stop("Block number expected to be an integer")
+        if(num > private$last_block) stop("Block number bigger than total number of blocks")
+        return(private$blocks[num])
+      }
+    },
 
-  validateItems <- function(items){
-    if (is.null(items)) {
-      message("No items to validate.")
-    } else {
-      for (i in 1:nrow(items)) {
-        i <- items[i, ]
-        ck <- as.character(sha512(paste(i$Id, i$Timestamp, i$Data, sep = "+")))
-        chk_result <- try(signature_verify(charToRaw(ck),
-                                           base64_decode(i$Check),
-                                           sha512,
-                                           keypair$pubkey),
-                          silent = TRUE)
-        if (class(chk_result) == "try-error"){
-          cat("Failed signature validation for transaction. Id: ", i$Id, "\n")
-          return(list(Result = FALSE, Details = i$Id, At = "Item"))
+    getPubKey = function() {
+      return(private$keypair$pubkey)
+    },
+
+    createBlock = function() {
+      if (private$last_block == 0L) {
+        # Need to mint genesis block
+        self$addItem("Welcome to Genesis Block")
+      }
+
+      if (is.null(private$items_pool)) {
+        message("No new items to add to block.")
+      } else {
+
+        it <- self$getItemPool()
+        private$resetItemPool()
+
+        id <- UUIDgenerate(FALSE)
+        ts <- as.POSIXlt(Sys.time(), "UTC")
+        prt1 <- ifelse(private$last_block == 0L, NA, private$blocks[[private$last_block]]$Header$Id)
+        prt2 <- ifelse(private$last_block == 0L, NA, private$blocks[[private$last_block]]$Header$Check)
+        hash <- as.character(sha512(paste(it$Check, collapse = "+")))
+        ck_str <- paste(id, ts, prt1, prt2, hash, sep="+")
+        ck <- signature_create(charToRaw(ck_str), sha512, private$keypair)
+
+        block <- list(Header = list(Seq = private$last_block + 1,
+                                    Id = id,
+                                    Timestamp = ts,
+                                    Parent = prt1,
+                                    Content = hash,
+                                    Check = base64_encode(ck)),
+                      Body = it)
+
+        private$blocks <- append(private$blocks, list(block))
+        private$last_block <- private$last_block + 1
+        return(list(Result = TRUE, Id = id, Seq = private$last_block))
+      }
+    },
+
+    validateItems = function(items){
+      if (is.null(items)) {
+        message("No items to validate.")
+      } else {
+        for (i in 1:nrow(items)) {
+          i <- items[i, ]
+          ck <- as.character(sha512(paste(i$Id, i$Timestamp, i$Data, sep = "+")))
+          chk_result <- try(signature_verify(charToRaw(ck),
+                                             base64_decode(i$Check),
+                                             sha512,
+                                             private$keypair$pubkey),
+                            silent = TRUE)
+          if (class(chk_result) == "try-error"){
+            cat("Failed signature validation for transaction. Id: ", i$Id, "\n")
+            return(list(Result = FALSE, Details = i$Id, At = "Item"))
+          }
         }
       }
-    }
-  return(list(Result = TRUE, Details = NA, At = "Item"))
-  }
+      return(list(Result = TRUE, Details = NA, At = "Item"))
+    },
 
-  validateBlock <- function(num) {
+    validateBlock = function(num) {
 
-    body_val <- validateItems(blocks[[num]]$Body)
-    if (body_val$Result) {
-      # Body items validated, lets check header integrity
-      ck_str <- paste(blocks[[num]]$Header$Id,
-                      blocks[[num]]$Header$Timestamp,
-                      ifelse(num == 1, NA, blocks[[num - 1]]$Header$Id),
-                      ifelse(num == 1, NA, blocks[[num - 1]]$Header$Check),
-                      blocks[[num]]$Header$Content,
-                      sep="+")
-      chk_result <- try(signature_verify(charToRaw(ck_str),
-                                         base64_decode(blocks[[num]]$Header$Check),
-                                         sha512,
-                                         keypair$pubkey),
-                        silent = TRUE)
-      if (class(chk_result) == "try-error"){
-        cat("Failed signature validation for block. Id: ", blocks[[num]]$Header$Id, "\n")
-        return(list(Result = FALSE, Details = blocks[[num]]$Header$Id, At = "Block"))
+      body_val <- self$validateItems(private$blocks[[num]]$Body)
+      if (body_val$Result) {
+        # Body items validated, lets check header integrity
+        ck_str <- paste(private$blocks[[num]]$Header$Id,
+                        private$blocks[[num]]$Header$Timestamp,
+                        ifelse(num == 1, NA, private$blocks[[num - 1]]$Header$Id),
+                        ifelse(num == 1, NA, private$blocks[[num - 1]]$Header$Check),
+                        private$blocks[[num]]$Header$Content,
+                        sep="+")
+        chk_result <- try(signature_verify(charToRaw(ck_str),
+                                           base64_decode(private$blocks[[num]]$Header$Check),
+                                           sha512,
+                                           private$keypair$pubkey),
+                          silent = TRUE)
+        if (class(chk_result) == "try-error"){
+          cat("Failed signature validation for block. Id: ", private$blocks[[num]]$Header$Id, "\n")
+          return(list(Result = FALSE, Details = private$blocks[[num]]$Header$Id, At = "Block"))
+        } else {
+          return(list(Result = TRUE, Details = NA, At = "Block"))
+        }
       } else {
-        return(list(Result = TRUE, Details = NA, At = "Block"))
+        return(body_val)
       }
-    } else {
-      return(body_val)
-    }
-  }
+    },
 
-  validateChain <- function() {
-    for (b in 1:last_block) {
-      blk_val <- validateBlock(b)
-      if (!blk_val$Result) {
-        cat("Chain validation failed at block", b, "\n")
-        return(blk_val)
+    validateChain = function() {
+      for (b in 1:private$last_block) {
+        blk_val <- self$validateBlock(b)
+        if (!blk_val$Result) {
+          cat("Chain validation failed at block", b, "\n")
+          return(blk_val)
+        }
       }
+      return(list(Result = TRUE, Details = NA, At = "Chain"))
     }
-    return(list(Result = TRUE, Details = NA, At = "Chain"))
-  }
+  ),
 
   # Internal functions
-  resetItemPool <- function() {
-    items_pool <<- NULL
-  }
+  private = list(
 
-  # Initialize chain
-  createBlock()
-  return(list(addItem = addItem,
-              getItemPool = getItemPool,
-              createBlock = createBlock,
-              getBlocks = getBlocks,
-              getPubKey = getPubKey,
-              validateChain = validateChain))
-}
+    items_pool = NULL,
 
-rChain <- rChainEnc()
-rm(rChainEnc)
+    blocks = list(),
+
+    last_block = 0L,
+
+    keypair = NULL,
+
+    resetItemPool = function() {
+      private$items_pool <<- NULL
+    }
+  ),
+  cloneable = FALSE,
+
+  lock_class = TRUE)
